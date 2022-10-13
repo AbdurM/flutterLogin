d27aabc3c0755043a3e97e5f1d60ffe56db94049
Abdur Mohammed <rafayk66@gmail.com>
2022-10-13 14:36:13 +0530

    Added button animation and improvements

--- lib/main.dart
+++ lib/main.dart
@@ -5,99 +5,165 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Login',
       theme: ThemeData.dark(),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  void OnPressLogin() {}
+  final _passwordTextController = TextEditingController();
+  var _isPasswordInvalid = false;
+  double _topMarginOfSignInButton = 200;
+
+  void onPressLogin() {
+    setState(() {
+      if (_isPasswordInvalid) {
+        _topMarginOfSignInButton = _topMarginOfSignInButton == 200 ? 20 : 200;
+        return;
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
       body: Container(
-        margin: const EdgeInsets.only(top: 100),
+        margin: const EdgeInsets.only(top: 80),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
+          children: [
             const Padding(
-                padding: EdgeInsets.only(top: 15.0, left: 30),
-                child: Text(
-                  "Let's sign you in.",
-                  style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
-                )),
+              padding: EdgeInsets.only(top: 15, left: 30),
+              child: Text(
+                "Let's sign you in.",
+                style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+              ),
+            ),
             const Padding(
-              padding: EdgeInsets.only(top: 15.0, left: 30),
-              child: Text("Welcome back.\nYou've been missed!",
-                  style: TextStyle(fontSize: 35)),
+              padding: EdgeInsets.only(top: 15, left: 30),
+              child: Text(
+                "Welcome back.\nYou've been missed!",
+                style: TextStyle(fontSize: 35),
+              ),
             ),
             Padding(
               padding: const EdgeInsets.only(top: 80, left: 30, right: 30),
               child: TextField(
+                enableSuggestions: false,
+                autocorrect: false,
                 decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(15)),
-                    hintText: "Phone, emai or username"),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 20, left: 30, right: 30),
-              child: TextField(
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(15)),
-                    hintText: "Password"),
-              ),
-            ),
-            Padding(
-                padding: EdgeInsets.only(top: 250.0),
-                child: Center(
-                  child: RichText(
-                    textAlign: TextAlign.center,
-                    text: (TextSpan(
-                        style: TextStyle(fontSize: 15),
-                        children: <TextSpan>[
-                          TextSpan(text: "Don't have an account? "),
-                          TextSpan(
-                              text: 'Register',
-                              style: TextStyle(fontWeight: FontWeight.bold))
-                        ])),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15),
                   ),
-                )),
-            Padding(
-              padding: const EdgeInsets.only(top: 30.0, left: 40, right: 40),
-              child: ElevatedButton(
-                onPressed: OnPressLogin,
-                child: Text(
-                  "Sign in",
-                  style: TextStyle(color: Colors.black, fontSize: 20),
+                  hintText: 'Phone, email or username',
                 ),
-                style: ElevatedButton.styleFrom(
-                    shape: new RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(15)),
-                    minimumSize: const Size.fromHeight(60),
-                    primary: Colors.white),
               ),
-            )
+            ),
+            SizedBox(
+              height: 100,
+              child: Padding(
+                padding: const EdgeInsets.only(top: 20, left: 30, right: 30),
+                child: TextField(
+                  obscureText: true,
+                  enableSuggestions: false,
+                  autocorrect: false,
+                  controller: _passwordTextController,
+                  onChanged: (value) {
+                    setState(() {
+                      _isPasswordInvalid = _passwordTextController.text !=
+                          '12345678'; // for the sake of example
+                    });
+                  },
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    hintText: 'Password',
+                    errorText: _isPasswordInvalid ? 'Incorrect Password' : null,
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 330,
+              width: MediaQuery.of(context).size.width,
+              child: Stack(
+                children: [
+                  AnimatedPositioned(
+                    height: 140,
+                    width: MediaQuery.of(context).size.width,
+                    top: _topMarginOfSignInButton,
+                    duration: const Duration(seconds: 1),
+                    curve: Curves.fastOutSlowIn,
+                    child: GestureDetector(
+                      onTap: onPressLogin,
+                      child: Column(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(bottom: 20),
+                            child: Center(
+                              child: RichText(
+                                textAlign: TextAlign.center,
+                                text: const TextSpan(
+                                  style: TextStyle(fontSize: 15),
+                                  children: [
+                                    TextSpan(text: "Don't have an account? "),
+                                    TextSpan(
+                                      text: 'Register',
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(
+                              horizontal: 20,
+                            ),
+                            child: ElevatedButton(
+                              onPressed: _isPasswordInvalid ? null : () => {},
+                              style: ElevatedButton.styleFrom(
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(15),
+                                ),
+                                minimumSize: const Size.fromHeight(60),
+                                backgroundColor: Colors.white,
+                                disabledBackgroundColor: Colors.grey,
+                              ),
+                              child: const Text(
+                                'Sign in',
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 20,
+                                ),
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
